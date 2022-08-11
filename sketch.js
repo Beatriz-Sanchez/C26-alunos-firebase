ec72f376167a417f59396f2acde632581604b49c
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-08-11 20:03:33 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,25 @@
 var bola;
 
 function setup() {
-  createCanvas(400, 400);
-  bola = createSprite(200, 200, 10, 10);
-  bola.shapeColor = "crimson";
+    createCanvas(400, 400);
+    bola = createSprite(200, 200, 10, 10);
+    bola.shapeColor = "crimson";
 }
 
 function draw() {
-  background(0);
-  drawSprites();
+    background(0);
+    drawSprites();
 
-if (keyDown("up")){
-  mudarPos(0,-3);
-}
-if (keyDown("down")){
-  mudarPos(0,3);
-}
-if (keyDown("left")){
-  mudarPos(-3,0);
-}
-if (keyDown("right")){
-  mudarPos(3,0);
-}
-}
-function mudarPos(x,y){
-  bola.x = bola.x + x;
-  bola.y = bola.y + y;
+    if (keyDown("up")) {
+        bola.y -= 3;
+    }
+    if (keyDown("down")) {
+        bola.y += 3;
+    }
+    if (keyDown("left")) {
+        bola.x -= 3;
+    }
+    if (keyDown("right")) {
+        bola.y += 3;
+    }
 }
